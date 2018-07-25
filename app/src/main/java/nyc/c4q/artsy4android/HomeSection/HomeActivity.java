30b997265b6a4bcb4f2d8c8ea31c41e6f327a67b
Bouabacar <boubacarbah@ac.c4q.nyc>
2018-07-24 20:33:08 -0400

    added circleImage lib to gradle

--- app/src/main/java/nyc/c4q/artsy4android/HomeSection/HomeActivity.java
+++ app/src/main/java/nyc/c4q/artsy4android/HomeSection/HomeActivity.java
@@ -1,6 +1,5 @@
 package nyc.c4q.artsy4android.HomeSection;
 
-import android.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
