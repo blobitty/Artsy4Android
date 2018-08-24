f2b5395484bb9e89792f05252f14ac66f98179a0
Bouabacar <boubacarbah@ac.c4q.nyc>
2018-08-24 19:56:23 -0400

    finished artist_view_pager. Needs UI

--- app/src/main/java/nyc/c4q/artsy4android/ArtistPage_Activity.java
+++ app/src/main/java/nyc/c4q/artsy4android/ArtistPage_Activity.java
@@ -1,15 +1,21 @@
 package nyc.c4q.artsy4android;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import nyc.c4q.artsy4android.R;
+import nyc.c4q.artsy4android.artistpage.ArtistPager_Fragment;
 
 public class ArtistPage_Activity extends AppCompatActivity {
-
+    ArtistPager_Fragment artistPager_fragment = new ArtistPager_Fragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_page_);
+
+        FragmentTransaction fTransaction = getSupportFragmentManager().beginTransaction();
+        fTransaction.replace(R.id.artist_pageContainer, artistPager_fragment);
+        fTransaction.commit();
     }
 }
--- app/src/main/java/nyc/c4q/artsy4android/artistpage/ArtistPager_Fragment.java
+++ app/src/main/java/nyc/c4q/artsy4android/artistpage/ArtistPager_Fragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import nyc.c4q.artsy4android.R;
 
@@ -49,8 +54,40 @@ public class ArtistPager_Fragment extends Fragment {
         setupViewPager(viewPager);
     }
 
-    public void setupViewPager(ViewPager viewPager) {
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new About_Fragment(), "ABOUT");
+        adapter.addFragment(new Works_Fragment(), "WORKS");
+        adapter.addFragment(new ShowsFragment(), "SHOWS");
+        viewPager.setAdapter(adapter);
     }
 
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) { super(manager); }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+    }
 
 }
