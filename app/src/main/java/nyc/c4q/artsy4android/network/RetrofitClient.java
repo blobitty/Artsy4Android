c84bbc856e507daa6cf5f581a1ce549f13342081
Bouabacar <boubacarbah@ac.c4q.nyc>
2019-03-02 18:56:40 -0500

    add okhttp and rxjava to retrofit client

--- app/src/main/java/nyc/c4q/artsy4android/network/RetrofitClient.java
+++ app/src/main/java/nyc/c4q/artsy4android/network/RetrofitClient.java
@@ -1,6 +1,15 @@
 package nyc.c4q.artsy4android.network;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -8,14 +17,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RetrofitClient {
-    private static Retrofit retrofit;
-    private static final String BASE_URL = "https://api.artsy.net/api/";
 
-    public static Retrofit getRetrofit(){
-        if (retrofit == null) return retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofit(String BASE_URL){
+
+        if (retrofit == null) {
+            //OKHttp Client
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .build();
+            //Retrofit client with OKHttp Client
+            retrofit = new Retrofit.Builder()
+                    .client(client)
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                    .build();
+        }
         return retrofit;
     }
 }
