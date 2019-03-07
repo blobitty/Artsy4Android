6f5a039b774a3ba6feae30ef4e122be065a507aa
Bouabacar <boubacarbah@ac.c4q.nyc>
2019-03-07 09:51:36 -0500

    refactored SplashCall -> SplashRepository

--- app/src/main/java/nyc/c4q/artsy4android/SplashPage/SplashActivity.java
+++ app/src/main/java/nyc/c4q/artsy4android/SplashPage/SplashActivity.java
@@ -8,26 +8,22 @@ import android.os.Bundle;
 
 import nyc.c4q.artsy4android.MainActivity;
 
-public class SplashActivity extends AppCompatActivity implements SplashCall {
+public class SplashActivity extends AppCompatActivity {
 
-    SplashTokenCall splash_xappToken = new SplashTokenCall();
+    //SplashTokenRepository splash_xappToken = new SplashTokenRepository();
     private static final String SHARED_PREFS_KEY = "sharedPrefs";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences tokenSharedPrefs = getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE);
-        createToken(SHARED_PREFS_KEY, tokenSharedPrefs);
+        //createToken(SHARED_PREFS_KEY, tokenSharedPrefs);
 
     }
-
-    @Override
     public void createToken(String SHARED_PREFS_KEY, SharedPreferences sharedPreferences) {
-          splash_xappToken.createToken(SHARED_PREFS_KEY,sharedPreferences);
+          //splash_xappToken.createToken(SHARED_PREFS_KEY,sharedPreferences);
           Intent intent = new Intent(SplashActivity.this, MainActivity.class);
           startActivity(intent);
           finish();
     }
-    //Makes a retrofit call to obtain xapp token and save xapp string to sharedpreferences
-
 }
--- app/src/main/java/nyc/c4q/artsy4android/SplashPage/SplashTokenRepository.java
+++ app/src/main/java/nyc/c4q/artsy4android/SplashPage/SplashTokenRepository.java
@@ -1,39 +1,28 @@
 package nyc.c4q.artsy4android.SplashPage;
 
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import io.reactivex.Single;
 import nyc.c4q.artsy4android.models.Constants;
 import nyc.c4q.artsy4android.models.Token;
-import nyc.c4q.artsy4android.network.RetrofitClient;
 import nyc.c4q.artsy4android.network.RetrofitService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-import static nyc.c4q.artsy4android.controller.ArtistsListAdapter.TAG;
-
-class SplashTokenCall {
-    private Token token;
+class SplashTokenRepository {
     private RetrofitService retrofitService;
 
 
-    SplashTokenCall(RetrofitService retrofitService, Token token){
+    SplashTokenRepository(RetrofitService retrofitService){
         retrofitService = this.retrofitService;
-        token = this.token;
     }
     Single<Token> createToken(String SHARED_PREFS_KEY, final SharedPreferences tokenSharedPrefs) {
-        //RetrofitService retroService = RetrofitClient.getRetrofit(Constants.BASEURL).create(RetrofitService.class);
+        return retrofitService.createToken(Constants.client_id, Constants.client_secret);
+    }
+}
 
-        Single<Token> tokenStream = retrofitService.createToken(Constants.client_id, Constants.client_secret);
+
 //        //token = tokenStream unwrapSingle to access response.body();
 //        String xappToken = token.getToken();
 //                    Log.d(TAG, "onResponse: XAPP= " + xappToken);
 //                    SharedPreferences.Editor editor = tokenSharedPrefs.edit();
 //                    editor.putString(Constants.TOKEN_KEY, xappToken);
 //                    editor.apply();
-        return tokenStream;
-
-    }
-}
